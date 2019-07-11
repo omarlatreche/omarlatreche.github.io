1bc0b875ed194ba9a69473f2780d608f300bf269
Omar Latreche <omar1933@hotmail.co.uk>
2019-07-11 18:46:08 +0100

    added desktop check

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+	if($(window).width() > 600){
+		$('body').empty();
+		$('body').append('<div class="mobile_only">\
+						 	<h1>:-(</h1>\
+						  	<p>Unfortunately the desktop version of this website is under construction.</p>\
+						  	<p>Try viewing it on your smartphone.</p>\
+						  </div>');
+	}
 	$('#getstarted').click(function(){
 		$('html,body').animate({
 			scrollTop: $('#1').offset().top},'slow');
